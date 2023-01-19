f66952e7de21796eeb2567d7f56f1b46fd6ebf34
orangecat <shaunchua.edu@gmail.com>
2023-01-19 23:40:06 +0800

    Add solutions for 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let start = num1;
+    let end = num2;
+    
+    // Check if num1 and num2 are numbers
+    if (typeof(num1) !== 'number' || typeof(num2) !== 'number') {
+        return 'ERROR';
+    }
 
+    // Check if num1 or num2 are negative numbers
+    if (num1 <0 || num2 < 0) {
+        return 'ERROR';
+    }
+
+    // If num1 > num2
+    if (num1 > num2) {
+        start = num2;
+        end = num1;
+    }
+
+    // Sum
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
+    
 };
 
 // Do not edit below this line
