a12463470a8a5041f0112c513709ae061b0262fb
orangecat <shaunchua.edu@gmail.com>
2023-01-25 00:54:31 +0800

    Add solutions for 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,34 @@
-const palindromes = function () {
+const palindromes = function (testString) {
+    let stringArray = Array.from(testString);
+    let filteredString = '';
+    let filteredArray = [];
+    let reversed = '';
+
+    // Create string without punctation
+    for (let i = 0; i < stringArray.length; i++) {
+
+        // Use ReGex to filter for alphabets only
+        if (stringArray[i].toLowerCase().match(/[a-z]/)) {
+            filteredString += stringArray[i].toLowerCase();
+        }
+    }
+
+
+    for (let i = stringArray.length - 1; i >= 0; i--) {
+        
+        // Use ReGex to filter for alphabets only
+        if (stringArray[i].toLowerCase().match(/[a-z]/)) {
+            reversed += stringArray[i].toLowerCase();
+        }
+    }
+
+    // console.log(reversed);
+
+    if (reversed === filteredString) {
+        return true; 
+    } else {
+        return false;
+    }
 
 };
 
