da32b51ea43c2cea4afa486d4c51e7850c78f0ba
orangecat <shaunchua.edu@gmail.com>
2023-01-25 00:26:54 +0800

    Add solutions for 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	let sum = 0;
+
+	for (number of numbers) {
+		sum += number;
+	}
+
+	return sum;
 };
 
-const multiply = function() {
+const multiply = function(numbers) {
+	let product = 1;
 
+	for (number of numbers) {
+		product *= number;
+	}
+
+	return product;
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	return num**power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = num;
+
+	if (num === 0) {
+		return 1;
+	}
+
+	for (let i = num - 1; i > 0; i--) {
+		result *= i;
+	}
+
+	return result;
 };
 
 // Do not edit below this line
