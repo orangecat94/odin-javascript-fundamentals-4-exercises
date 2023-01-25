5e699e7e6351f7ca7dd441154f04318fa6d3c044
orangecat <shaunchua.edu@gmail.com>
2023-01-25 15:40:41 +0800

    Add solutions for 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    let oldestPerson = null;
+    let oldestAge = 0;
+    let personAge = 0;
+
+    for (person of people) {
+
+        // Get person age
+        if (isNaN(person.yearOfDeath)) {
+            let year = new Date();
+            personAge = year.getFullYear() - person.yearOfBirth;
+
+        } else {
+            personAge = person.yearOfDeath - person.yearOfBirth;
+        }
+
+        // Update oldest age, and oldest person 
+        if (personAge > oldestAge) {
+            oldestAge = personAge;
+            oldestPerson = person;
+        }
+    }
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
