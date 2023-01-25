46bfc2d1cf558ffebe3613ededbc543fe3e66b37
orangecat <shaunchua.edu@gmail.com>
2023-01-25 11:33:10 +0800

    Add solutions for 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+
+    let previous = 1;
+    let current = 1;
+    let next = 0;
+
+    // Reject negative numbers
+    if (member <= 0) {
+        return 'OOPS';
+    }
+
+    // Return 1 if member is 1 or 2
+    if (member <= 2) {
+        return 1;
+    }
+
+    for (let i = 3; i <= member; i++) {
+        
+        next = current + previous;
+        previous = current;
+        current = next;
+    }
+
+    return current;  
 
 };
 
